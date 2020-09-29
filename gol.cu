9059e0f4f1986b8318ea622b877842c5f303ae4b
Paddy Polson <paddypolson@gmail.com>
2020-09-30 09:41:15 +1300

    A whole bunch of stuff

--- gol.cu
+++ gol.cu
@@ -4,9 +4,21 @@
 #include <time.h>
 
 #include <iostream>
+#include <fstream>
 #include <tuple>
 #include <random>
 #include <functional>
+#include <chrono>
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
 __device__ bool getCell( bool* input, int x, int y, int* size ) {
     if ( x < 0 ) { x = x + size[1]; }
@@ -25,19 +37,44 @@ __device__ int getNeighbourCount( bool* input, int x, int y, int* size ) {
     return count;
 }
 
-__global__ void simulate( bool* input, bool* output, int* size, int steps ) {
+__global__ void simulate( bool* input, bool* output, int width, int height, int steps ) {
 
     int index = threadIdx.x;
     int stride = blockDim.x;
 
-    if ( input[ index ] ) {
-        // Cell is alive
-
-    } else {
-        // Cell is dead
-        if ( getNeighbourCount( input, size ))
+    if ( index >= ( width * height )) {
+        // Index out of range
+        printf("Out of range: %d\n", index);
+        return;
     }
 
+    // Find X and Y
+    int y = index / width;
+    int x = index % width;
+    int size[2] = {height, width};
+    //printf("X: %d, Y: %d\n", x, y);
+
+    int count = getNeighbourCount( input, x, y, size );
+
+    if ( input[ index ] ) {
+        //printf("alive");
+        // Cell is alive
+        if ( count == 2 || count == 3 ) {
+            // Cell has 2 or 3 neighbours
+            output[ index ] = true;
+        } else {
+            output[ index ] = false;
+        }
+    } else {
+        //printf("dead");
+        // Cell is dead
+        if ( count == 3 ) {
+            // Cell has exactly 3 neighbours
+            output[ index ] = true;
+        } else {
+            output[ index ] = false;
+        }
+    }
 }
 
 /*
@@ -72,6 +109,7 @@ static void show_usage(std::string name)
               << "\t-r, --random\t\tInstead start with a randomized starting state, provide a seed, 0 will set a random seed" << std::endl
               << "\t-o, --output\t\tOptionally save the final state as a file" << std::endl
               << "\t-s, --steps\t\tThe number of simulation step to take" << std::endl
+              << "\t-p, --play\t\ttOptionally play the simulation in the console" << std::endl
               << std::endl;
 }
 
@@ -81,9 +119,14 @@ int main( int argc, char* argv[] ) {
     char* input;
     char* output;
     bool isRandom = false;
+    std::ifstream infile;
+    std::ofstream outfile;
+    bool play = false;
     int seed;
     int steps = 0;
-    int size[2] = {10, 10};
+    int size[2] = {20, 10};
+    int width, height;
+    int gridSize = size[1] * size[0] * sizeof(bool*);
 
     bool* grid;
     bool* d_in;        // The read-only input array for kernel
@@ -94,7 +137,7 @@ int main( int argc, char* argv[] ) {
         exit( EXIT_FAILURE );
     }
 
-    while (( opt = getopt(argc, argv, "hi:o:r:s:" )) != -1 ) {
+    while (( opt = getopt(argc, argv, "hi:o:r:s:p" )) != -1 ) {
         switch ( opt ) {
 
         case 'h':
@@ -118,6 +161,10 @@ int main( int argc, char* argv[] ) {
         case 's':
             steps = atoi(optarg);
             break;
+        
+        case 'p':
+            play = true;
+            break;
 
         default: /* '?' */
         show_usage( argv[0] );
@@ -126,9 +173,9 @@ int main( int argc, char* argv[] ) {
     }
 
     // Init empty grid
-    grid = (bool*) malloc( size[1] * size[0] * sizeof(bool*) );
-    cudaMalloc( &d_in, size[1] * size[0] * sizeof(bool*) );
-    cudaMalloc( &d_out, size[1] * size[0] * sizeof(bool*) );
+    grid = (bool*) malloc( gridSize );
+    gpuErrchk( cudaMalloc( &d_in, gridSize ) );
+    gpuErrchk( cudaMalloc( &d_out, gridSize ) );
 
     for ( int y = 0; y < size[1]; y++ ) {
         for ( int x = 0; x < size[0]; x++ ) {
@@ -137,20 +184,45 @@ int main( int argc, char* argv[] ) {
     }
 
     if ( isRandom ) {
-        auto gen = std::bind(   std::uniform_int_distribution<>( 0,1 ),
-                                std::default_random_engine() );
+        if ( ! seed ) {
+            seed = std::chrono::steady_clock::now().time_since_epoch().count();
+        }
+        std::default_random_engine engine(seed);
+        std::uniform_int_distribution<> boolGen( 0, 1 );
         for ( int y = 0; y < size[1]; y++ ) {    
             for ( int x = 0; x < size[0]; x++ ) {
-                grid[ y * size[1] + x ] = gen();
+                grid[ y * size[1] + x ] = boolGen( engine );
             }
         }
+    } else {
+        std::ifstream infile("thefile.txt");
     }
 
     printGrid( grid, size );
 
-    simulate<<< 1, 1 >>>( d_in, d_out, size, steps );
-    // Wait for GPU to finish before accessing on host
-    cudaDeviceSynchronize();
+    gpuErrchk( cudaMemcpy ( d_in, grid, gridSize, cudaMemcpyHostToDevice ) );
+
+    for (int step = 0; step < steps; step++) {
+
+        simulate<<< 1, 100 >>>( d_in, d_out, size[0], size[1], steps );
+
+        if ( play ) {
+            gpuErrchk( cudaMemcpy ( grid, d_out, gridSize, cudaMemcpyDeviceToHost ) );
+            gpuErrchk( cudaDeviceSynchronize() );
+            clear();
+            printGrid( grid, size );
+            sleep( 1 );
+        }
+
+        gpuErrchk( cudaMemcpy ( d_in, d_out, gridSize, cudaMemcpyHostToHost ) );
+    }
+
+    if ( !play ) {
+        // Wait for GPU to finish before accessing on host
+        gpuErrchk( cudaMemcpy ( grid, d_out, gridSize, cudaMemcpyDeviceToHost ) );
+        gpuErrchk( cudaDeviceSynchronize() );
+        printGrid( grid, size );
+    }
 
     // Clean up memory allocations
     free( grid );
